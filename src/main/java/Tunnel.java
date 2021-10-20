fcaa0457da4dbc9ca5c3182203da5865fca42b01
KiraSokol <kira99sav1122@gmail.com>
2021-10-21 00:15:06 +0400

    HW

---
+++ src/main/java/Tunnel.java
@@ -0,0 +1,21 @@
+public class Tunnel extends Stage {
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+            MainClass.semaphore.acquire();
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(c.getName() + " закончил этап: " + description);
+            MainClass.semaphore.release();
+        }
+    }
+}
